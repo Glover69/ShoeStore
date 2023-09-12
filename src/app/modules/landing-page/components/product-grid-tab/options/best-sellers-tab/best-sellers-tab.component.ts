a6ef9d504d0328cf08dd4e09f88ef225bfcdcd09
Glover69 <iamglover02@gmail.com>
2023-09-12 20:36:25 +0000

    best sellers section - mobile

--- src/app/modules/landing-page/components/product-grid-tab/options/best-sellers-tab/best-sellers-tab.component.ts
+++ src/app/modules/landing-page/components/product-grid-tab/options/best-sellers-tab/best-sellers-tab.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+export interface CardData {
+  productImg: string,
+  productName: string,
+  productPrice: string,
+}
+
 @Component({
   selector: 'app-best-sellers-tab',
   templateUrl: './best-sellers-tab.component.html',
   styleUrls: ['./best-sellers-tab.component.scss']
 })
 export class BestSellersTabComponent {
+
+  card : CardData[] = [
+    {productImg: '/assets/images/Nike/nike-vapor-women-1.jpeg', productName: 'Nike Jordan MVP', productPrice: '$56.00'},
+    {productImg: '/assets/images/Nike/nike-vapor-women-1.jpeg', productName: 'Nike Jordan MVP', productPrice: '$56.00'},
+    {productImg: '/assets/images/Nike/nike-vapor-women-1.jpeg', productName: 'Nike Jordan MVP', productPrice: '$56.00'},
+    {productImg: '/assets/images/Nike/nike-vapor-women-1.jpeg', productName: 'Nike Jordan MVP', productPrice: '$56.00'},
+    {productImg: '/assets/images/Nike/nike-vapor-women-1.jpeg', productName: 'Nike Jordan MVP', productPrice: '$56.00'},
+    {productImg: '/assets/images/Nike/nike-vapor-women-1.jpeg', productName: 'Nike Jordan MVP', productPrice: '$56.00'},
+    {productImg: '/assets/images/Nike/nike-vapor-women-1.jpeg', productName: 'Nike Jordan MVP', productPrice: '$56.00'},
+    {productImg: '/assets/images/Nike/nike-vapor-women-1.jpeg', productName: 'Nike Jordan MVP', productPrice: '$56.00'},
+
+  ]
 
 }
