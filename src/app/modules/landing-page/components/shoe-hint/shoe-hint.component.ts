d2d46388f960efcc49046604bcbb820e9962fdd0
Glover69 <iamglover02@gmail.com>
2023-09-12 14:30:03 +0000

    hero section -  mobile

---
+++ src/app/modules/landing-page/components/shoe-hint/shoe-hint.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shoe-hint',
+  templateUrl: './shoe-hint.component.html',
+  styleUrls: ['./shoe-hint.component.scss']
+})
+export class ShoeHintComponent {
+
+}
--- src/app/modules/landing-page/landing-page.module.ts
+++ src/app/modules/landing-page/landing-page.module.ts
@@ -3,10 +3,12 @@ import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { SharedModule } from 'src/shared/shared.module';
 import { LandingPageComponent } from './landing-page.component';
 import { CommonModule } from '@angular/common';
+import { ShoeHintComponent } from './components/shoe-hint/shoe-hint.component';
 
 @NgModule({
   declarations: [
-    LandingPageComponent
+    LandingPageComponent,
+    ShoeHintComponent
   ],
   imports: [
     CommonModule,
